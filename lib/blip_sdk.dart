0ec23687eeb61e521959d546800deefdeca14a54
Andre Rossi <andre.rossi@take.net>
2022-03-14 17:06:38 -0300

    added clientError

--- lib/blip_sdk.dart
+++ lib/blip_sdk.dart
@@ -5,3 +5,4 @@ export 'src/application.dart' show Application;
 export 'src/client.dart' show Client;
 export 'src/client_builder.dart' show ClientBuilder;
 export 'src/extensions/media/media.extension.dart' show MediaExtension;
+export 'src/client_error.dart' show ClientError;
